175b1524b3fa271e8a8392644240fdda9ec6e669
SanMumumu <1257663033@qq.com>
2026-03-14 08:38:58 +0800

    v5.4 Infer

--- run.sh
+++ run.sh
@@ -6,3 +6,13 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main_rgb.py --n_gpus 8 --batch_size 
 echo "QWer1234." | sshfs -p 39115 wangsen@218.200.126.238:/mnt/data/wangsen/TriFlow/results_e2e /mnt/nodestor/ws/UniWM/ULE/results_remote/115 -o password_stdin,allow_other
 echo "iairws" | sshfs -p 39117 wangsen@218.200.126.238:/mnt/data/wangsen/World_model/ULE/results_e2e /mnt/nodestor/ws/UniWM/ULE/results_remote/117 -o password_stdin,allow_other
 tensorboard --logdir_spec=TriFlow_115:/mnt/nodestor/ws/UniWM/ULE/results_remote/115,UniWM_Local:/mnt/nodestor/ws/UniWM/ULE/results_woCFG,WorldModel_117:/mnt/nodestor/ws/UniWM/ULE/results_remote/117 --port 6006 
+
+
+
+python eval_benchmark.py \
+    --config configs/model.yaml \
+    --ckpt results_woCFG/20260313_033450_CITYSCAPES_RGB_42/ckpt_0110000.pt \
+    --future_frames 28 \
+    --NFE 50 \
+    --traj 1 \
+    --output ./results_benchmark_110k
